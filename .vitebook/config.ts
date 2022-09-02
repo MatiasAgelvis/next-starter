de7d8d92d14133978f35fbb510446f0ea840644a
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-01 20:46:32 -0400

    test: Add Vitebook

---
+++ .vitebook/config.ts
@@ -0,0 +1,20 @@
+import { clientPlugin, defineConfig } from "@vitebook/client/node";
+import { preactPlugin } from "@vitebook/preact/node";
+import {
+  defaultThemePlugin,
+  DefaultThemeConfig,
+} from "@vitebook/theme-default/node";
+
+export default defineConfig<DefaultThemeConfig>({
+  include: ["src/**/*.story.{jsx,tsx}"],
+  plugins: [
+    preactPlugin({ appFile: "App.tsx" }),
+    clientPlugin(),
+    defaultThemePlugin(),
+  ],
+  site: {
+    title: "Aliados",
+    description: "Main App",
+    theme: {},
+  },
+});
